55fb947b0cbc20e261cc7401004585bbd8234d7b
takashabe <takashabe.public@gmail.com>
2016-10-21 18:18:19 +0900

    implement stringer exercise

--- stringer.go
+++ stringer.go
@@ -5,9 +5,14 @@ import "fmt"
 type IPAddr [4]byte
 
 // expected: IPAddr{1,  2,  3,  4} => "1.2.3.4"
-func (i IPAddr) String() string {
-	// TODO: 配列ループさせて値を取り出して結合
-	return fmt.Sprintf("")
+func (addr IPAddr) String() string {
+	var ret string
+	for _, v := range addr {
+		ret = ret + fmt.Sprint(v) + "."
+	}
+	ret = ret[:len(ret)-1]
+	ret = "\"" + ret + "\""
+	return ret
 }
 
 func main() {
