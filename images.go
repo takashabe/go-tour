1fe04a8385bb245c8460c0300eac67f71f647d8b
takashabe <takashabe.public@gmail.com>
2016-10-28 23:18:50 +0900

    implemented images.go

--- images.go
+++ images.go
@@ -4,18 +4,22 @@ package main
 
 import (
 	"golang.org/x/tour/pic"
+	"image"
 	"image/color"
 )
 
 type Image struct{}
 
 func (i Image) ColorModel() color.Model {
+	return color.RGBA64Model
 }
 
-func (i Image) Bounds() Rectangle {
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 100, 100)
 }
 
 func (i Image) At(x, y int) color.Color {
+	return color.RGBA{0, 0, 255, 255}
 }
 
 func main() {
